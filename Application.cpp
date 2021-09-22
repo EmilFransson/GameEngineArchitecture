dff06ecf8127f0491e85e6e2e79a83ab2a63e24b
EmilHogstedt <emil.hogstedt@hotmail.com>
2021-09-22 17:31:07 +0200

    Implemented StackAllocator testing.

--- Application.cpp
+++ Application.cpp
@@ -2,13 +2,15 @@
 #include "Application.h"
 #include "System.h"
 #include "RenderCommand.h"
+#include "StackAllocator.h"
 
 #define TOKENPASTE(x, y) x ## y
 #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
 #define PROFILE_FUNC Profiler TOKENPASTE2(profiler, __LINE__) (__FUNCTION__, [&](ProfileMetrics profileMetrics) {m_ProfileMetrics.push_back(std::move(profileMetrics));})
 #define PROFILE_SCOPE(scopeName) Profiler TOKENPASTE2(profiler, __LINE__) (scopeName, [&](ProfileMetrics profileMetrics) {m_ProfileMetrics.push_back(std::move(profileMetrics));})
 
-
+#define MEGA 1000000
+#define GIGA 1000000000
 Application::Application() noexcept
 	: m_Running{true}
 {
@@ -20,6 +22,8 @@ Application::Application() noexcept
 
 void Application::Run() noexcept
 {
+	//Send in the size in bytes
+	StackAllocator::CreateAllocator(GIGA);
 	while (m_Running)
 	{
 		static const FLOAT color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
@@ -70,6 +74,7 @@ void Application::DisplayProfilingResults() noexcept
 
 void Application::AllocateCubes(uint32_t nrOfCubes) noexcept
 {
+	//Poolallocator
 	std::string str = "Application::AllocateCubes (" + std::to_string(nrOfCubes) + ") - PoolAllocator";
 	{
 		PROFILE_SCOPE(str);
@@ -88,4 +93,30 @@ void Application::AllocateCubes(uint32_t nrOfCubes) noexcept
 			delete cube;
 		}
 	}
+
+	//Stackallocator.
+	str = "Application::AllocateCubes (" + std::to_string(nrOfCubes) + ") - StackAllocator";
+	{
+		PROFILE_SCOPE(str);
+		for (uint32_t i{ 0u }; i < nrOfCubes; i++)
+		{
+			Cube* cube = StackAllocator::GetInstance()->New<Cube>();
+		}
+		StackAllocator::GetInstance()->CleanUp();
+	}
+	str = "Application::AllocateCubes (" + std::to_string(nrOfCubes) + ") - Normal stack";
+	{
+		PROFILE_SCOPE(str);
+		std::vector<Cube*> cubeVector;
+		for (uint32_t i{ 0u }; i < nrOfCubes; i++)
+		{
+			cubeVector.push_back(new Cube());
+		}
+		for (uint32_t i{ 0u }; i < nrOfCubes; i++)
+		{
+			delete cubeVector[i];
+			cubeVector[i] = nullptr;
+		}
+	}
+
 }
